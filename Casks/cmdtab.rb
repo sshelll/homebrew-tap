2e63739a1d1ae7b7c4c50de63a1ea1b91bed0434
kayce <shaojiale.gitignore@icloud.com>
2025-12-04 01:16:28 +0800

    bump cmdtab to v0.1.0

---
+++ Casks/cmdtab.rb
@@ -0,0 +1,16 @@
+cask "cmdtab" do
+  version "0.1.0"
+  sha256 "e501a36f055913e8ba32853f648730263525913c6248429731407dbad42ac171"
+
+  url "https://github.com/sshelll/CmdTab/releases/download/v#{version}/CmdTab.dmg"
+  name "CmdTab"
+  desc "MacOS Cmd-Tab alternative"
+  homepage "https://github.com/sshelll/CmdTab"
+
+  app "CmdTab.app"
+
+  zap trash: [
+    "~/Library/Preferences/com.sshelll.CmdTab.plist",
+    "~/Library/Application Support/CmdTab",
+  ]
+end
