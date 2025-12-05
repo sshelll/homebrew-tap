5dfe95fac820fd09c4bbcb608b32ea1763d0826d
kayce <shaojiale.gitignore@icloud.com>
2025-12-05 15:00:06 +0800

    update cmdtab sha256

--- Casks/cmdtab.rb
+++ Casks/cmdtab.rb
@@ -1,6 +1,6 @@
 cask "cmdtab" do
   version "0.1.2"
-  sha256 "f0c3294804b1df2c10430208df1868a5c5a8a3409f8c240dddae5feae4bc4fc3"
+  sha256 "f6df5b61c55d0c6161fd8dbc25616568993d9a5d6e3d282809fde0fee0218cae"
 
   url "https://github.com/sshelll/CmdTab/releases/download/v#{version}/CmdTab.dmg"
   name "CmdTab"
