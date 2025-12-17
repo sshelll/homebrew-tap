884bf98bda216cd9c24750679f97efb889092b25
kayce <shaojiale.gitignore@icloud.com>
2025-12-17 16:58:39 +0800

    bump cmdtab to v0.1.6

--- Casks/cmdtab.rb
+++ Casks/cmdtab.rb
@@ -1,6 +1,6 @@
 cask "cmdtab" do
-  version "0.1.5"
-  sha256 "b6d84351bcdd00e52d733135c3cc28ced38e284b8ae4c45335576d87432e16a1"
+  version "0.1.6"
+  sha256 "cf791f1af31e219fa96cf3faa3e02c83577ce251ae1ab00fbc8a22a654f5564e"
 
   url "https://github.com/sshelll/CmdTab/releases/download/v#{version}/CmdTab.dmg"
   name "CmdTab"
